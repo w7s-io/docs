ef37e2ac0c886f1765fc1cfc7ee35eba7e6054cc
W7S <git@w7s.io>
2026-05-27 16:41:01 -0300

    Remove docs navbar logo

--- src/theme/Navbar/Logo/index.tsx
+++ src/theme/Navbar/Logo/index.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
 
-const W7S_LOGO = 'https://github.com/w7s-io.png';
-
 export default function NavbarLogo(): React.ReactNode {
   return (
     <a className="navbar__brand" href="https://www.w7s.io/">
-      <img className="navbar__logo-avatar" src={W7S_LOGO} alt="W7S logo" />
       <b className="navbar__title text--truncate">W7S</b>
     </a>
   );
